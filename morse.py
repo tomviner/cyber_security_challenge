f524cb33800a23726541c11ffd23dc94b5e96659
TomV <tom@viner.tv>
2013-12-23 02:34:27 +0000

    simple Morse to plaintext encode/decoders

---
+++ morse.py
@@ -0,0 +1,62 @@
+import re
+
+# From http://svn.python.org/projects/stackless/trunk/Demo/scripts/morse.py
+CHAR_TO_MORSE = {
+    "'": '.----.', '(': '-.--.-', ')': '-.--.-', ',': '--..--',
+    '-': '-....-', '.': '.-.-.-', '/': '-..-.', '0': '-----',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', ':': '---...', ';': '-.-.-.', '?': '..--..',
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
+    'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '_': '..--.-',
+}
+
+MORSE_TO_CHAR = {v:k for k,v in CHAR_TO_MORSE.items()}
+
+
+def decode_to_words(s):
+    """Takes a Morse code string and generates plaintext words"""
+    words = re.split(r'\s{2,}', s)
+    for i, word in enumerate(words):
+        yield ''.join(MORSE_TO_CHAR.get(ch, ch) for ch in word.split(' '))
+
+def decode(s):
+    """Takes a Morse code string and returns plaintext"""
+    return ' '.join(decode_to_words(s))
+
+
+def encode_to_words(s):
+    """Takes a plaintext string and generates Morse code words
+
+    Morse letters are joined by a single space
+    """
+    words = s.split(' ')
+    for i, word in enumerate(words):
+        yield ' '.join(CHAR_TO_MORSE.get(ch, ch) for ch in word)
+
+def encode(s):
+    """Takes a plaintext string and returns Morse code
+
+    Words are joined by a double space
+    """
+    return '  '.join(encode_to_words(s))
+
+
+# tests
+
+morse_sos = '... --- ...'
+def test_sos_decode():
+    assert decode(morse_sos) == 'SOS'
+def test_sos_encode():
+    assert encode('SOS') == morse_sos
+
+morse_12_23 = '.---- ..---  ...-- ....-'
+def test_words_encode():
+    assert encode('12 34') == morse_12_23
+def test_words_decode():
+    assert decode(morse_12_23) == '12 34'
